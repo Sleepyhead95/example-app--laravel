41fa7ce55cf267e34d13fce3fc0c9e7ad8b2f7b7
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2024-02-26 11:22:21 +0100

    user login functionality in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Auth\User;
 
 class UserController extends Controller
 {
@@ -11,5 +13,28 @@ class UserController extends Controller
     public function create()
     {
         return view('users.register');
+    }
+
+    // store new user
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', 'min:6'],
+        ]);
+
+        // hash password:
+        // bcrypt() is a built-in PHP function that hashes passwords
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        // create user:
+        $user = User::create($formFields);
+
+        // log user in:
+        auth()->login($user);
+
+        // redirect to home page after loggin in:
+        return redirect('/')->with('message', 'User created and logged in');
     }
 }
